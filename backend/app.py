c3522b4fa6b50288bcac8c2d10f312bf6466113c
debugwithpriyanka <bishtpriyanka1427@gmail.com>
2026-07-05 14:11:34 +0530

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from langchain_ollama import OllamaLLM
+
+app = Flask(__name__)
+CORS(app)
+
+llm = OllamaLLM(model="llama3")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    data = request.json
+    msg = data["message"]
+
+    response = llm.invoke(msg)
+
+    return jsonify({
+        "reply": response
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
