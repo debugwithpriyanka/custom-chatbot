48aa90b3611cb79fe2521f344f3f217edab1daaa
debugwithpriyanka <bishtpriyanka1427@gmail.com>
2026-07-07 20:29:41 +0530

    design professional sidebar UI

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,86 @@
 import "./Sidebar.css";
 
-function Sidebar(){
-    return(
+import {
+    FiPlus,
+    FiMessageSquare,
+    FiSettings,
+    FiUser
+} from "react-icons/fi";
+
+function Sidebar() {
+    return (
+
         <aside className="sidebar">
-            Sidebar
+
+            <div>
+
+                <h2 className="logo">
+                    🤖 AI Assistant
+                </h2>
+
+                <button className="new-chat">
+
+                    <FiPlus />
+
+                    New Chat
+
+                </button>
+
+            </div>
+
+            <div className="history">
+
+                <h4>Recent Chats</h4>
+
+                <div className="history-item">
+
+                    <FiMessageSquare />
+
+                    Hello AI
+
+                </div>
+
+                <div className="history-item">
+
+                    <FiMessageSquare />
+
+                    React Help
+
+                </div>
+
+                <div className="history-item">
+
+                    <FiMessageSquare />
+
+                    Python
+
+                </div>
+
+            </div>
+
+            <div className="bottom">
+
+                <div className="menu">
+
+                    <FiSettings />
+
+                    Settings
+
+                </div>
+
+                <div className="menu">
+
+                    <FiUser />
+
+                    Priyanka
+
+                </div>
+
+            </div>
+
         </aside>
-    )
+
+    );
 }
 
 export default Sidebar;
